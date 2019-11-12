acbaaddeb93ceaa4818bd1655930e21e3ae759c2
kong0827 <1351882069@qq.com>
2019-11-13 00:09:46 +0800

    onetomany

---
+++ Spring-Data-Jpa-day07/src/main/java/com/kxj/dao/LinkManDao.java
@@ -0,0 +1,11 @@
+package com.kxj.dao;
+
+import com.kxj.entity.LinkMan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * 联系人的dao接口
+ */
+public interface LinkManDao extends JpaRepository<LinkMan,Long>, JpaSpecificationExecutor<LinkMan> {
+}
